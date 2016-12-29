a6f66b4ca1aaf24a01a80c181d468d5d4717e6c8
enujo <rexolua@naver.com>
2016-12-29 11:32:30 +0900

    Lesson06 의존성 낮추기

--- Lsesson06/src/spms/listeners/ContextLoaderListener.java
+++ Lsesson06/src/spms/listeners/ContextLoaderListener.java
@@ -8,29 +8,37 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
 
+import spms.controls.MemberAddController;
+import spms.controls.MemberListController;
 import spms.dao.MemberDao;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
-  @Override
-  public void contextInitialized(ServletContextEvent event) {
-    try {
-      ServletContext sc = event.getServletContext();
-      
-      InitialContext initialContext = new InitialContext();
-      DataSource ds = (DataSource)initialContext.lookup(
-          "java:comp/env/jdbc/studydb");
-      
-      MemberDao memberDao = new MemberDao();
-      memberDao.setDataSource(ds);
-      
-      sc.setAttribute("memberDao", memberDao);
-
-    } catch(Throwable e) {
-      e.printStackTrace();
-    }
-  }
-
-  @Override
-  public void contextDestroyed(ServletContextEvent event) {}
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		try {
+			ServletContext sc = event.getServletContext();
+			      
+			InitialContext initialContext = new InitialContext();
+			DataSource ds = (DataSource)initialContext.lookup("java:comp/env/jdbc/studydb");
+			  
+			MemberDao memberDao = new MemberDao();
+			memberDao.setDataSource(ds);
+			  
+			 // sc.setAttribute("memberDao", memberDao);
+			  
+			MemberListController memberListController = new MemberListController();
+			memberListController.setMemberDao(memberDao);
+			
+			sc.setAttribute("/member/list.do", memberListController);
+			  
+			sc.setAttribute("/member/add.do", new MemberAddController().setMemberDao(memberDao));
+		      
+		} catch(Throwable e) {
+		e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {}
 }
